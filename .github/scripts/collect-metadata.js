7e7f36cf38e9c3ab854a0293af9cfb8ab9638db5
RieN 7z <zrien7@gmail.com>
2025-11-16 18:05:03 +0800

    fix: update structure

--- .github/scripts/collect-metadata.js
+++ .github/scripts/collect-metadata.js
@@ -3,6 +3,21 @@ const path = require('path');
 const matter = require('gray-matter');
 const { execSync } = require('child_process');
 
+function getAllFolders(dir) {
+  let results = [];
+  const list = fs.readdirSync(dir);
+  list.forEach(file => {
+    const filePath = path.join(dir, file);
+    const stat = fs.statSync(filePath);
+    if (stat && stat.isDirectory()) {
+      if (file === 'node_modules') return;
+      if (file.startsWith(".")) return;
+      results.push(filePath);
+    }
+  });
+  return results;
+}
+
 function getAllFiles(dir, exts) {
   let results = [];
   const list = fs.readdirSync(dir);
@@ -12,8 +27,10 @@ function getAllFiles(dir, exts) {
     if (stat && stat.isDirectory()) {
       // 跳过 node_modules 目录
       if (file === 'node_modules') return;
+      if (file.startsWith(".")) return;
       results = results.concat(getAllFiles(filePath, exts));
     } else if (exts.includes(path.extname(file))) {
+      if (file.startsWith(".")) return;
       results.push(filePath);
     }
   });
@@ -40,33 +57,43 @@ function getLastCommitTimestamp(filePath) {
 
 function collectMetadata() {
   const root = path.resolve(process.cwd());
-  const files = getAllFiles(root, ['.md', '.mdx']);
-  const result = files.map(file => {
-    const raw = fs.readFileSync(file, 'utf8');
-    const { data } = matter(raw);
-    let name = data.title || path.basename(file);
-    let date;
-    let update_date;
-    if (data.date) {
-      const d = new Date(data.date);
-      date = d.getTime();
-    } else {
-      date = getFirstCommitTimestamp(file);
-    }
-    if (data.update_date) {
-      const d = new Date(data.date);
-      update_date = d.getTime(); 
-    } else {
-      update_date = getLastCommitTimestamp(file)
-    }
-    return {
-      id: path.basename(file),
-      name,
-      date,
-      update_date
-    };
-  });
-  fs.writeFileSync(path.join(__dirname, 'blog-metadata.json'), JSON.stringify(result, null, 2));
+  const folders = getAllFolders(root)
+  let result = []
+  for (const folder of folders) {
+    const files = getAllFiles(folder, ['.md', '.mdx']);
+    const metas = files.map(file => {
+      const raw = fs.readFileSync(file, 'utf8');
+      const { data } = matter(raw);
+      let name = data.title || path.basename(file);
+      let description = data.description
+      let type = folder.split("/")
+      type = type[type.length - 1]
+      let date;
+      let update_date;
+      if (data.date) {
+        const d = new Date(data.date);
+        date = d.getTime();
+      } else {
+        date = getFirstCommitTimestamp(file);
+      }
+      if (data.update_date) {
+        const d = new Date(data.date);
+        update_date = d.getTime();
+      } else {
+        update_date = getLastCommitTimestamp(file)
+      }
+      return {
+        id: path.basename(file),
+        name,
+        description,
+        date,
+        update_date,
+        type
+      };
+    });
+    result = result.concat(...metas)
+  }
+  fs.writeFileSync(path.join(__dirname, 'metadata.json'), JSON.stringify(result, null, 2));
 }
 
 collectMetadata();
